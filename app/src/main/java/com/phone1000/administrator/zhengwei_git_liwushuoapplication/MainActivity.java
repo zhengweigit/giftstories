f3da1a5236d344f3ed54c219790666e2c9b0c4d2
zhengweigit <zhengwei_git@163.com>
2016-06-27 17:12:12 +0800

    我的礼物说

---
+++ app/src/main/java/com/phone1000/administrator/zhengwei_git_liwushuoapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.phone1000.administrator.zhengwei_git_liwushuoapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
